788d04253f375ff6141b69db3141579e6b58f7ea
Mateus de Sousa Silva Santos <54694745+mateuso12@users.noreply.github.com>
2021-06-07 22:17:55 -0300

    Movies genres refactored :tada:

---
+++ src/hooks/useMoviesContext.tsx
@@ -0,0 +1,90 @@
+import { ReactNode, useContext, useEffect } from "react";
+import { useState } from "react";
+import { createContext } from "react";
+import { api } from "../services/api";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface MovieContextData {
+  selectedGenreId: number;
+  genres: GenreResponseProps[];
+  movies: MovieProps[];
+  selectedGenre: GenreResponseProps;
+  handleClickButton: (id: number) => void;
+}
+
+interface MoviesProviderProps {
+  children: ReactNode;
+}
+
+const MoviesContext = createContext<MovieContextData>({} as MovieContextData);
+
+export function MoviesProvider({ children }: MoviesProviderProps) {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>(
+    {} as GenreResponseProps
+  );
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    api
+      .get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`)
+      .then((response) => {
+        setMovies(response.data);
+      });
+
+    api
+      .get<GenreResponseProps>(`genres/${selectedGenreId}`)
+      .then((response) => {
+        setSelectedGenre(response.data);
+      });
+  }, [selectedGenreId]);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <MoviesContext.Provider
+      value={{
+        selectedGenreId,
+        genres,
+        movies,
+        selectedGenre,
+        handleClickButton,
+      }}
+    >
+      {children}
+    </MoviesContext.Provider>
+  );
+}
+
+export function useMoviesContext() {
+  const context = useContext(MoviesContext);
+
+  return context;
+}
